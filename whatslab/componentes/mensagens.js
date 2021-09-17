1aafc31b2cc3c863a292c2af6705f3ee2f6f0f56
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-09-17 00:58:21 -0300

    Criei a classe EnvioDeMensagens

--- whatslab/componentes/mensagens.js
+++ whatslab/componentes/mensagens.js
@@ -1,7 +1,51 @@
-class EnvioDeMensagens extends React.Component {
+class EnvioDeMensagens extends React.Component{
     state = {
-        mensagem:
+      postagem: [
+        valorInputPessoa= "",
+        valorInputMsm=""
+      ],
+       
+    };
+
+    adicionarNovaMensagem = () => {
+        const novaMensagem = {
+            usuario: this.state.valorInputPessoa,
+            msm: this.state.valorInputMsm,
+        };
+        this.setState({postagem: novaMensagem});
     }
-  }
-  
-  export default EnvioDeMensagens;
+
+    onChangeInputUsuario = (event) => {
+        this.setState({ valorInputUsuario: event.target.value });
+    }
+
+    onChangeInputMsm = (event) => {
+        this.setState({ valorInputMsm: event.target.value });
+    }
+
+    onClickEnviar = () => {};
+
+    render(){
+        console.log(this.state);
+        return (
+            <div>
+                <h1>Conversa do WhatsLab</h1>
+                <input
+                 placeholder={"Nome completo"}
+                 value={this.state.usuario}
+                 onChange={this.onChangeInputUsuario}
+                /> 
+                <input
+                placeholder={"Digite sua Mensagem"}
+                value={this.state.msm}
+                onChange={this.onChangeInputMsm}
+               />
+
+            </div>
+        )
+
+    }
+
+}
+
+export default EnvioDeMensagens;
--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,17 +1,11 @@
-import './App.css';
-// import EnvioDeMensagens from '../componentes/mensagens';
+// import './App.css';
+import React from 'react';
+import EnvioDeMensagens from '../componentes/Mensagens';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-          Learn React
-    
-      </header>
+     <EnvioDeMensagens/>
     </div>
   );
 }
