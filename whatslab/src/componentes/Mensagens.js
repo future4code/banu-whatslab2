dc93a3de2c8088013aa4de1d2f992daa811a7df9
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-09-17 22:13:31 -0300

    Adicionado onClickEnviar

--- whatslab/src/componentes/Mensagens.js
+++ whatslab/src/componentes/Mensagens.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react'
+import styled from "styled-components"
+
+
+
+
 
 class EnvioDeMensagens extends React.Component{
     state = {
-     usuario: '',
-     msm: ''
+        
+     usuario:'',
+     msm: '',
+     mensagem: []
+        
     };
 
     onChangeUsuario = (event) => {
@@ -12,27 +20,57 @@ class EnvioDeMensagens extends React.Component{
 
     onChangeMsm = (event) => {
         this.setState({ msm: event.target.value });
-
     }
+       onClickEnviar = () => {
+        if (this.state.usuario !== "" && this.state.msm !== "") {
+            const novaMensagem = {
+              usuarioValue: this.state.usuario,
+              msmValue: this.state.msm,
+            };
+            const novoArray = [novaMensagem, ...this.state.msm];
+            this.setState({ messagem: novoArray });
+            this.setState({ msm: "", usuario: "" }); //Area para resetar o campo mensagem e nome
+          } else {
+            alert("Por favor, preencha todos os campos!");
+          }
+        };
 
-    onClickEnviar = () => {
-        
-    };
+        //envio mensagem
+        enviarMensagem = (event) => {
+            if(event.key === "Enter"){
+                this.onClickEnviar();
+            }
+        };
 
+        deletMensagem = (index) => {
+            if(window.confirm("Tem certeza que quer remover essa mensagem?")){
+                const deleteMensagem = this.state.msm;
+                deleteMensagem.splice(index,1);
+                this.setState({msm: deleteMensagem});
+            }
+        };
 
     render(){
-        console.log(this.state);
         return (
            <div>
+               <div
+               mensagem={this.state.mensagem}
+               usuario={this.state.usuario}
+               msm={this.state.msm}
+               onDoubleClick={this.deletMensagem}
+               />
+               <div onKeyPress={this.enviarMensagem}/>
                <input
                  placeholder={'Nome completo'}
                  value={this.state.usuario}
                  onChange={this.onChangeUsuario}
+                 required
                 /> 
                 <input
                 placeholder={'Digite sua Mensagem'}
                 value={this.state.msm}
                 onChange={this.onChangeMsm}
+                required
                />
               <br />
             <button onClick={this.onClickEnviar}>Enviar</button>
