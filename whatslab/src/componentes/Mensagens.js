dfd7032577679f28bb7d4123d76e45c8c5015792
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-09-19 15:15:19 -0300

    Consegui imprimir as mensagens na tela

--- whatslab/src/componentes/Mensagens.js
+++ whatslab/src/componentes/Mensagens.js
@@ -2,12 +2,6 @@ import React from 'react';
 import styled from 'styled-components'
 
 
-const Container = styled.div`
-width:45vw;
-height:2cm;
-display:flex;
-`
-
 const UsuarioInput = styled.input`
   background-color: white;
   border-radius:5px;
@@ -32,84 +26,70 @@ const EnvioDeMsm = styled.button`
   font-weight:bold;
   font-size:16px;
 `
-const MsmLista = styled.div`
-background-color: red;
-border-radius:5px;
-border: 1px solid lightgray;
-heigth: 400px;
-margin: 3px 8px;
-width: 80px;
-`
 
 class EnvioDeMensagens extends React.Component{
     state = {
         
-     usuario:'',
-     msm: '',
+     usuarioDigitando:'',
+     msmDigitando: '',
      mensagem: []
         
     };
 
     onChangeUsuario = (event) => {
-        this.setState({ usuario: event.target.value });
+        this.setState({ usuarioDigitando: event.target.value });
     }
 
     onChangeMsm = (event) => {
-        this.setState({ msm: event.target.value });
+        this.setState({ msmDigitando: event.target.value });
     }
-       onClickEnviar = () => {
-        if (this.state.usuario !== "" && this.state.msm !== "") {
-            const novaMensagem = {
-              usuarioValue: this.state.usuario,
-              msmValue: this.state.msm,
-            };
-            const novoArray = [novaMensagem, ...this.state.msm];
-            this.setState({ mensagem: novoArray });
-            this.setState({ msm: "", usuario: "" }); //Area para resetar o campo mensagem e nome
-          } else {
-            alert("Por favor, preencha todos os campos!");
-          }
-        };
 
-        //envio mensagem
-        enviarMensagem = (event) => {
-            if(event.key === "Enter"){
-                this.onClickEnviar();
+    onClickEnviar = () => {
+      if (
+        this.state.usuarioDigitando !== "" && 
+        this.state.msmDigitando !== ""
+        ) {
+          this.setState({
+            msmDigitando: "", 
+            usuarioDigitando: ""
+          })
+          const novoArray = [
+            ...this.state.mensagem, 
+            {
+              usuarioValue: this.state.usuarioDigitando,
+              msmValue: this.state.msmDigitando
             }
-        };
-
-        // deletMensagem = (index) => {
-        //     if(window.confirm("Tem certeza que quer remover essa mensagem?")){
-        //         const deleteMensagem = this.state.msm;
-        //         deleteMensagem.splice(index,1);
-        //         this.setState({msm: deleteMensagem});
-        //     }
-        // };
+          ];
+          this.setState({ 
+            mensagem: novoArray
+          });
+      } else {
+        alert("Por favor, preencha todos os campos!");
+      }
+    }
 
     render(){
         return (
            <div>
-               <MsmLista
-               mensagem={this.state.mensagem}
-               usuario={this.state.usuario}
-               msm={this.state.msm}
-               />
-               <Container onKeyPress={this.enviarMensagem}/>
+              <div>
+                {this.state.mensagem.map((mensagem, index) => {
+                  return <p key={index}>{mensagem.usuarioValue}: {mensagem.msmValue} </p>
+                })}
+              </div>
                <UsuarioInput
                  placeholder={'Nome completo'}
-                 value={this.state.usuario}
+                 value={this.state.usuarioDigitando}
                  onChange={this.onChangeUsuario}
                 /> 
                 <MsmInput
                 placeholder={'Digite sua Mensagem'}
-                value={this.state.msm}
+                value={this.state.msmDigitando}
                 onChange={this.onChangeMsm}
                />
-              <br />
             <EnvioDeMsm onClick={this.onClickEnviar} type="submit">
               Enviar
             </EnvioDeMsm>
-            </div>
+          </div>
         )
 
     }
